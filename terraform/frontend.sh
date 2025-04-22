786706d6d85d6736a6f5d3b67c91da0f18f77cce
medasani <medasanicharan66@gmail.com>
2025-04-22 19:39:16 +0530

    01-frontend-deployment

--- terraform/frontend.sh
+++ terraform/frontend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-app_version=$1
+app_Version=$1
 dnf install ansible -y
 pip3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/MEDASANI-CHARAN/03-expense-ansible-roles.git frontend.yaml -e appVersion=$app_version
+ansible-pull -i localhost, -U https://github.com/MEDASANI-CHARAN/03-expense-ansible-roles.git frontend.yaml -e appVersion=$app_Version
